cca64491d0fe622be3eed9db95befcd09a80de07
JonathanIntrica <84321976+JonathanIntrica@users.noreply.github.com>
2024-03-15 14:06:28 +0000

    Implement data mutation

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { createInvoice } from "@/app/lib/actions";
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
@@ -8,9 +11,11 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 
+
 export default function Form({ customers }: { customers: CustomerField[] }) {
+
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -23,6 +28,9 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              required
+              onInvalid={(e: any) => e.currentTarget.setCustomValidity('Please select a customer')}
+              onChange={(e) => e.currentTarget.setCustomValidity('')}
             >
               <option value="" disabled>
                 Select a customer
@@ -51,6 +59,9 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                required
+                onInvalid={(e: any) => e.currentTarget.setCustomValidity('Please provide the invoice payment total')}
+                onChange={(e: any) => !!e.target.value && e.currentTarget.setCustomValidity('')}
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -70,6 +81,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   name="status"
                   type="radio"
                   value="pending"
+                  defaultChecked
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
                 <label
