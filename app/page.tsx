0c9e622f2663b8cac9a64c6ceecd005a8352985c
JonathanIntrica <84321976+JonathanIntrica@users.noreply.github.com>
2024-03-18 13:37:03 +0000

    Implement auth system

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { inter } from "@/app/ui/fonts";
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+
 import Link from 'next/link';
+
 import Image from "next/image";
 export default function Page() {
   return (
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { useEffect } from "react";
+import { useFormState } from 'react-dom';
 import { createInvoice } from "@/app/lib/actions";
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
@@ -13,9 +15,10 @@ import { Button } from '@/app/ui/button';
 
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(createInvoice, initialState);
 
-  return (
-    <form action={createInvoice}>
+  return (<form action={dispatch} aria-describedby="form-error">
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -28,9 +31,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
-              required
-              onInvalid={(e: any) => e.currentTarget.setCustomValidity('Please select a customer')}
-              onChange={(e) => e.currentTarget.setCustomValidity('')}
+              aria-label="Select invoice customer"
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -42,6 +44,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.customerId &&
+              state.errors.customerId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
         </div>
 
@@ -59,17 +69,24 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                required
-                onInvalid={(e: any) => e.currentTarget.setCustomValidity('Please provide the invoice payment total')}
-                onChange={(e: any) => !!e.target.value && e.currentTarget.setCustomValidity('')}
+                aria-describedby="amount-error"
+                aria-label="select invoice amount"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+            <div id="amount-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.amount &&
+                state.errors.amount.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
             </div>
           </div>
         </div>
 
         {/* Invoice Status */}
-        <fieldset>
+        <fieldset aria-describedby="status-error">
           <legend className="mb-2 block text-sm font-medium">
             Set the invoice status
           </legend>
@@ -81,8 +98,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   name="status"
                   type="radio"
                   value="pending"
-                  defaultChecked
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-label="Set invoice as pending"
                 />
                 <label
                   htmlFor="pending"
@@ -97,6 +114,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   name="status"
                   type="radio"
                   value="paid"
+                  aria-label="Set invoice as paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
                 <label
@@ -108,16 +126,28 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
             </div>
           </div>
+          <div id="status-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.status &&
+              state.errors.status.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
         </fieldset>
       </div>
-      <div className="mt-6 flex justify-end gap-4">
+    <div id="form-error" aria-live="polite" aria-atomic="true">
+      {!!state.message && <p className="mt-2 text-sm text-right text-red-500">{state.message}</p>}
+    </div>
+    <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/invoices"
+          aria-label="Cancel"
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
           Cancel
         </Link>
-        <Button type="submit">Create Invoice</Button>
+        <Button type="submit" aria-label="Create Invoice">Create Invoice</Button>
       </div>
     </form>
   );
